7de7cc81f49452fcd83fbcc6569c9dfb88278855
alejandro garcia <cgarciao@unbosque.edu.co>
2021-04-26 17:05:01 -0500

    Update db.php

--- persistencia/db.php
+++ persistencia/db.php
@@ -1,14 +1,18 @@
 <?php
+/**
+ *    $mysqli = new mysqli("localhost","root","","chibchaweb");
+ *  $mysqli1 = new mysqli("localhost","root","","chibchaweb");
+ *  $mysqli2 = new mysqli("localhost","root","","chibchaweb");
+ * 
+ * 
+ */
 
-   $mysqli = new mysqli("localhost","root","","chibchaweb");
-   $mysqli1 = new mysqli("localhost","root","","chibchaweb");
-   $mysqli2 = new mysqli("localhost","root","","chibchaweb");
     
     
-/** 
- *   $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
- *   $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
- *   $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
 
-*/
+    $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
+    $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
+    $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
+
+
 ?>
