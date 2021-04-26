24e014e5ef592919ef966f908e7345e5c2b2a1a3
alejandro garcia <cgarciao@unbosque.edu.co>
2021-04-25 21:30:11 -0500

    regreso a local para poder modificar

--- persistencia/db.php
+++ persistencia/db.php
@@ -1,4 +1,10 @@
 <?php
+/** 
+ *  $mysqli = new mysqli("localhost","root","","chibchaweb");
+ *  $mysqli1 = new mysqli("localhost","root","","chibchaweb");
+ *  $mysqli2 = new mysqli("localhost","root","","chibchaweb");
+    */
+    
 
     $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
     $mysqli = new mysqli("chibchaweb.mysql.database.azure.com","adminchibcha@chibchaweb","ChibchaWeb2021.","chibchaweb");
